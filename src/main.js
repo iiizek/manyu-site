65e304854aafd5f98b799356e84d7e7f02cbbfd6
Anton Gudenko <antong702@gmail.com>
2025-02-20 15:00:36 +0300

    fix build

--- src/main.js
+++ src/main.js
@@ -2,8 +2,6 @@ import "@/assets/main.scss";
 import { createApp } from "vue";
 import { createPinia } from "pinia";
 import { createHead } from "@unhead/vue";
-import { GesturePlugin } from "@vueuse/gesture";
-import { MotionPlugin } from "@vueuse/motion";
 
 import App from "./App.vue";
 import router from "./router";
@@ -12,8 +10,6 @@ const app = createApp(App);
 
 app.use(createPinia());
 app.use(createHead());
-app.use(MotionPlugin);
-app.use(GesturePlugin);
 app.use(router);
 
 app.mount("#app");
