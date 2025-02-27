b66d8578cef54669f7b3d25adfd26da46d5fe13d
Anton Gudenko <antong702@gmail.com>
2025-02-27 16:38:21 +0300

    add footer content

--- src/components/RootFooter.vue
+++ src/components/RootFooter.vue
@@ -1,3 +1,8 @@
+<script setup>
+import MapIcon from "@/components/icons/MapIcon.svg?component";
+import PhoneIcon from "@/components/icons/PhoneIcon.svg?component";
+</script>
+
 <template>
   <footer :class="$style.container">
     <router-link :class="$style.link" to="/about">
@@ -7,6 +12,15 @@
     <p :class="$style.text">
       MANYU | Since 2020 concept basic clothing<br />OWN PREMIUM PRODUCTION FROM
       DAGESTAN.
+    </p>
+
+    <p :class="[$style.text, $style.links]">
+      <a href="tel:+79285665046" target="_blank"
+        ><phone-icon /> +7 (928) 566 50-46</a
+      >
+      <a href="geo://42.974373,47.496283" target="_blank"
+        ><map-icon /> г. Махачкала, ул. Абубакарова, 48</a
+      >
     </p>
   </footer>
 </template>
@@ -51,5 +65,30 @@
   font-weight: var.$fw-regular;
   font-size: 2.4rem;
   color: var.$c-neutral-3;
+}
+
+.links {
+  display: flex;
+  gap: 2.5rem;
+
+  @media (width < 768px) {
+    flex-direction: column;
+  }
+
+  & > a {
+    display: flex;
+    align-items: center;
+    gap: 1.2rem;
+    font-weight: var.$fw-regular;
+    font-size: 2.4rem;
+    color: var.$c-neutral-2;
+    text-decoration: none;
+    transition: 0.2s color;
+
+    &:hover {
+      color: var.$c-accent;
+      text-decoration: underline;
+    }
+  }
 }
 </style>
